d2cf674d275a25c347748c55d18be9771edc880c
Василий Балтахинов <ptil.79.79@mail.ru>
2023-10-23 21:30:34 +0800

    Поставил доску и фигуры

--- ChessGenerator/ChessGenerator/Form1.Designer.cs
+++ ChessGenerator/ChessGenerator/Form1.Designer.cs
@@ -28,20 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panelBoard = new System.Windows.Forms.Panel();
+            this.pictureBoard = new System.Windows.Forms.PictureBox();
+            this.panelBoard.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoard)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panelBoard
+            // 
+            this.panelBoard.Controls.Add(this.pictureBoard);
+            this.panelBoard.Location = new System.Drawing.Point(37, 34);
+            this.panelBoard.Name = "panelBoard";
+            this.panelBoard.Size = new System.Drawing.Size(439, 420);
+            this.panelBoard.TabIndex = 0;
+            // 
+            // pictureBoard
+            // 
+            this.pictureBoard.Location = new System.Drawing.Point(43, 49);
+            this.pictureBoard.Name = "pictureBoard";
+            this.pictureBoard.Size = new System.Drawing.Size(331, 318);
+            this.pictureBoard.TabIndex = 0;
+            this.pictureBoard.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(604, 465);
+            this.ClientSize = new System.Drawing.Size(705, 630);
+            this.Controls.Add(this.panelBoard);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Chess Generator";
+            this.panelBoard.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoard)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelBoard;
+        private System.Windows.Forms.PictureBox pictureBoard;
     }
 }
 
--- ChessGenerator/ChessGenerator/Form1.cs
+++ ChessGenerator/ChessGenerator/Form1.cs
@@ -6,14 +6,53 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using ChessImages;
+using ChessSystem.Structure;
+using ChessSystem.Painting;
 
 namespace ChessGenerator
 {
     public partial class Form1 : Form
     {
+        AlphaPieces alpha;
+        PiecePosition alphaPosition;
+        PiecePaint alphaPaint;
+
+        AlphaField alf;
+        FieldPosition alfPosition;
+        FieldPaint alfPaint;
+
         public Form1()
         {
             InitializeComponent();
+            int wp = 8 + 6;
+            int e2 = 3 + (1 << 3);
+            int e4 = 3 + (3 << 3);
+            int d5 = 4 + (4 << 3);
+            int wr = 8 + 3;
+
+            alpha = new AlphaPieces();
+            panelBoard.BackgroundImage = alpha.Board();
+            panelBoard.Size = new Size(alpha.BoardSize(), alpha.BoardSize());
+            pictureBoard.Size = new Size(alpha.BoardSize() - alpha.BordSize(), alpha.BoardSize() - alpha.BordSize());
+            pictureBoard.Location = new Point(alpha.BordSize() / 2, alpha.BordSize() / 2);
+            pictureBoard.BackColor = Color.Transparent;
+            
+            alf = new AlphaField();
+
+            alfPosition = new FieldPosition();
+            //alfPosition.Add((d5 << 2) + 3);
+            alfPaint = new FieldPaint(alf);
+
+            alphaPosition = new PiecePosition();
+            //alphaPosition.Add(wr + (d5 << 5));
+            alphaPosition.Initialize();
+            alphaPaint = new PiecePaint(alpha);
+
+            LayerPaint alphaLayer = new LayerPaint(alfPaint.DrawFields(alfPosition.Items), alphaPaint.DrawPosition(alphaPosition.Items));
+
+            pictureBoard.Image = alphaLayer.Image();
         }
     }
 }
+;
